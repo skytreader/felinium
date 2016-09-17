b5b90aca7b37dc01004e5835096f2e1b76925766
Chad Estioco <chadestioco@gmail.com>
2016-09-18 06:45:52 +0800

    kfinecomment

--- felinium/multielements.py
+++ felinium/multielements.py
@@ -4,8 +4,11 @@ import time
 if __name__ == "__main__":
     driver = webdriver.Firefox()
     driver.get("http://127.0.0.1:16981/multielements.html")
-    clowns = driver.find_element_by_class_name("clown")
     time.sleep(3)
+    clowns = driver.find_element_by_class_name("clown")
     clowns.send_keys("haha")
+    #clowns = driver.find_elements_by_class_name("clown")
+    #for clown in clowns:
+    #    clown.send_keys("haha")
     time.sleep(3)
     driver.close()
