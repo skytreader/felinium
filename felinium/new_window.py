e122b914672072cfb43cf23c92855e3a2641dbfc
Chad Estioco <chadestioco@gmail.com>
2016-09-18 07:12:01 +0800

    haxmuhaha

--- felinium/new_window.py
+++ felinium/new_window.py
@@ -1,12 +1,18 @@
 from selenium import webdriver
 import time
 
+def alternate_approach(driver):
+    link = driver.find_element_by_class_name("link-dotted")
+    linky = link.get_attribute("href")
+    driver.get(linky)
+
 if __name__ == "__main__":
     driver = webdriver.Firefox()
     driver.get("https:/www.kalibrr.com/job-board")
-    driver.find_element_by_class_name("link-dotted").click()
-    print driver.window_handles
-    for handle in driver.window_handles:
-        driver.switch_to_window(handle)
+    alternate_approach(driver)
+    #driver.find_element_by_class_name("link-dotted").click()
+    #print driver.window_handles
+    #for handle in driver.window_handles:
+    #    driver.switch_to_window(handle)
     time.sleep(3)
     driver.close()
